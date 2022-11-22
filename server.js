23a44f9e026d5f34e8edecc6ad0adf454bccf4c8
jamarboledas <jamarboledas@gmail.com>
2022-11-22 07:19:43 +0100

    added acceptance job

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/status', (req, res) => {
+  res.send();
 });
 
 app.listen(PORT, HOST, () => {
