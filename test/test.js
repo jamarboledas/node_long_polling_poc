58465075836610ae7db86bebfc308e81b5f70c97
jamarboledas <jamarboledas@gmail.com>
2022-11-23 07:52:59 +0100

    health check acceptance test with mocha

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+
+describe('Health Check',()=>{
+  it('should return 200', (done) => {
+  chai.request('http://localhost:3000')
+  .get('/status')
+  .end( function(err,res){
+  console.log(res.body)
+  expect(res).to.have.status(200);
+  done();
   });
-});
+  });
+ });
